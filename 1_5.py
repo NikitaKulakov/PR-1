6f64aba3fcaa0cb8e0038f4deb5f1b7620fd9c90
NikitaKulakov <91449546+NikitaKulakov@users.noreply.github.com>
2021-10-26 10:14:26 +0300

    Add files via upload

---
+++ 1_5.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+n=int(input('Введите число:'))
+n=n+n**2+n**3+n**4+n**5
+print(n)
---
+++ 1_6.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+x=int(input('Введите X:'))
+y=int(input('Введите Y:'))
+z=y
+y=x
+x=z
+print('X=',x,' Y=',y)
+
